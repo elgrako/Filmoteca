025bac977bd972ca1bcfa8455d406b28741c41e0
elgrako <elgraco363@gmail.com>
2024-11-05 17:49:36 +0100

    1.1
    Primer ejercicio "About Activity" en desarrollo

---
+++ app/src/main/java/com/example/filmoteca_lucascandela/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.example.filmoteca_lucascandela;
+
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AboutActivity {
+    TextView creador;
+    ImageView imgabout;
+    Button webButton;
+    Button soporteButton;
+    Button volverButton;
+}
