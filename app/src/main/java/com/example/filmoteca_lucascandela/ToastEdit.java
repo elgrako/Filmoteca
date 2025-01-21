1ad0bc8c94beb4ab95ca0ee49f5e780dbfe82959
elgrako <elgraco363@gmail.com>
2025-01-21 14:39:55 +0100

    punto 15 Toast personalizado, clase y xmls creados (falta añadirlo al programa)

---
+++ app/src/main/java/com/example/filmoteca_lucascandela/ToastEdit.java
@@ -0,0 +1,24 @@
+package com.example.filmoteca_lucascandela;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ToastEdit {
+
+    public void showCustomToast(Context context, String message) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View layout = inflater.inflate(R.layout.toast_personalizado, null);
+
+        TextView text = layout.findViewById(R.id.toast_message);
+        text.setText(message);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+}
